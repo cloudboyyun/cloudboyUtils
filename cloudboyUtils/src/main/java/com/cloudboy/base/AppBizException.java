8e0836ed4b4010f86a8564d56c90a1bf67ec54b0
cloudboyyun <yun.xia2006@hotmail.com>
2014-11-02 13:54:38 +0800

    add 2 exception classes

---
+++ cloudboyUtils/src/main/java/com/cloudboy/base/AppBizException.java
@@ -0,0 +1,70 @@
+package com.cloudboy.base;
+
+import java.text.MessageFormat;
+
+public class AppBizException extends Exception {
+	public static final long serialVersionUID = 0x01;
+
+	private String code;
+
+	private Object[] args;
+
+	private String textMessage;
+
+	public AppBizException() {
+	}
+
+	private static String formatMessage(String msg, Object... args) {
+		return MessageFormat.format(msg, args);
+	}
+
+	public AppBizException(String code, String msg) {
+		super(code + ": " + msg);
+		this.code = code;
+	}
+	
+	public AppBizException(String code, String msg, Object... args) {
+		super(code + ": " + formatMessage(msg, args));
+		this.code = code;
+		this.args = args;
+	}
+
+	public AppBizException(String code, String msg, Throwable cause) {
+		super(code + ": " + msg, cause);
+		this.code = code;
+	}
+
+	public AppBizException(String code, String msg, Object[] args, Throwable cause) {
+		super(code + ": " + formatMessage(msg, args), cause);
+		this.code = code;
+		this.args = args;
+	}
+
+	public AppBizException(Throwable cause) {
+		super(cause);
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String string) {
+		code = string;
+	}
+
+	public Object[] getArgs() {
+		return args;
+	}
+
+	public void setArgs(Object[] objects) {
+		args = objects;
+	}
+
+	public String getTextMessage() {
+		return textMessage;
+	}
+
+	public void setTextMessage(String textMessage) {
+		this.textMessage = textMessage;
+	}
+}
---
+++ cloudboyUtils/src/main/java/com/cloudboy/base/AppRTException.java
@@ -0,0 +1,78 @@
+package com.cloudboy.base;
+
+import java.text.MessageFormat;
+
+
+public class AppRTException extends RuntimeException {
+	public static final long serialVersionUID = 0;
+	
+	/**
+	 * Error Code
+	 */
+	private String code;
+
+	/**
+	 * Exception parameters
+	 */
+	private Object[] args;
+
+	/**
+	 * Exception massage
+	 */
+	private String textMessage;
+
+	private static String formatMessage(String msg, Object[] args) {
+		return MessageFormat.format(msg, args);
+	}
+	
+	public AppRTException(String code, String msg) {
+		super(code + ": " + msg);
+		this.code = code;
+	}
+
+	public AppRTException(String code, String msg, Throwable cause) {
+		super(code + ": " + msg, cause);
+		this.code = code;
+	}
+
+	public AppRTException(String code, Object[] args, String msg) {
+		super(code + ": " + formatMessage(msg, args));
+		this.code = code;
+		this.args = args;
+	}
+
+	public AppRTException(String code, Object[] args, String msg, Throwable cause) {
+		super(code + ": " + formatMessage(msg, args), cause);
+		this.code = code;
+		this.args = args;
+	}
+
+	public AppRTException(Throwable cause) {
+		super(cause);
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String string) {
+		code = string;
+	}
+
+	public Object[] getArgs() {
+		return args;
+	}
+
+	public void setArgs(Object[] objects) {
+		args = objects;
+	}
+
+	public String getTextMessage() {
+		return textMessage;
+	}
+
+	public void setTextMessage(String textMessage) {
+		this.textMessage = textMessage;
+	}
+
+}
